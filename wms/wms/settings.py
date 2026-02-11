14a62099a60dd5a738955364f3129dfe27ff1c65
kamleshrebari <kamleshrebari275@gmail.com>
2026-02-11 13:21:15 +0530

    fix static css

--- wms/wms/settings.py
+++ wms/wms/settings.py
@@ -20,13 +20,14 @@ ALLOWED_HOSTS = ['*']
 
 # ================= STATIC =================
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / "static"
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # ================= MEDIA =================
 MEDIA_URL = '/media/'
